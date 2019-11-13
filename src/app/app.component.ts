ff7e8bd897094c9492cb13fe0c4fef1a85631bee
Ajay Marathe <ajay.marathe@xebecindia.com>
2019-11-13 19:37:18 +0530

    added tooltip, and popover

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+declare var $;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent  implements OnInit {
   title = 'ng-supine';
+
+  ngOnInit() {
+    this.EnablePopOver();
+    this.EnablePopOver();
+  }
+
+
+  EnableTooltip(){
+    $(function () {
+      $('[data-toggle="tooltip"]').tooltip();
+    });
+  }
+
+  EnablePopOver(){
+    $(function () {
+      $('[data-toggle="popover"]').popover();
+    });
+  }
 
 }
