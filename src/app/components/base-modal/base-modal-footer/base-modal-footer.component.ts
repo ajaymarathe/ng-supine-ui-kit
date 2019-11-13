4d64bcd219761a255206b7e8d90f3125f2684e53
Ajay Marathe <ajay.marathe@xebecindia.com>
2019-11-13 12:21:12 +0530

    base-spinner, base-modal functionality done!

---
+++ src/app/components/base-modal/base-modal-footer/base-modal-footer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-base-modal-footer',
+  templateUrl: './base-modal-footer.component.html',
+  styleUrls: ['./base-modal-footer.component.css']
+})
+export class BaseModalFooterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/components/base-spinner/base-spinner.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-base-spinner',
+  templateUrl: './base-spinner.component.html',
+  styleUrls: ['./base-spinner.component.css']
+})
+export class BaseSpinnerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
